c993b22476c7a4ce874e36848b493709d28773b5
landinguy <1029573528@qq.com>
2019-09-21 12:40:15 +0800

    update

--- src/test/kotlin/com/landinguy/kotlin/KotlinApplicationTests.kt
+++ src/test/kotlin/com/landinguy/kotlin/KotlinApplicationTests.kt
@@ -25,8 +25,42 @@ class KotlinApplicationTests {
 
     @Test
     fun test() {
-        val list = userService.find()
-        log.info("size#${list.size}")
+//        val list = userService.find()
+//        log.info("size#${list.size}")
+
+//        Result(0, "ok", "data1").let {
+//            println(it)
+//            it.msg = "error"
+//            it.data = "data2"
+//            println(it)
+//        }
+
+        /*** let、run、with 返回lambda表达式的值 ； also、apply 返回该对象 ***/
+        val language = mutableListOf("java", "kotlin", "python")
+
+        val result = language.also {
+            log.info("first item is ${it.first()}")
+        }.apply {
+            add("groovy")
+            log.info("size#$size")
+            add("scala")
+            log.info("size#$size")
+        }.let {
+            log.info("$it")
+            it
+        }.run {
+            add("c")
+            log.info("size#$size")
+            this
+        }
+
+        with(result) {
+            add("go")
+            log.info("result$this")
+            log.info("language$language")
+        }
+
+
     }
 
 }
